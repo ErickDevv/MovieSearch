0c74721e3954d18e830acc6b8e2816784dfd5a2e
Erick Devv <111036821+ErickDevv@users.noreply.github.com>
2023-12-11 16:55:38 -0600

    Update searchMovie.spec.ts

--- test/e2e/searchMovie.spec.ts
+++ test/e2e/searchMovie.spec.ts
@@ -4,5 +4,8 @@ test("Search for a movie", async ({ page }) => {
   await page.goto("http://localhost:5173/");
   await page.getByPlaceholder("Your favorite movie").click();
   await page.getByPlaceholder("Your favorite movie").fill("Hulk");
-  await page.getByRole("img", { name: "The Incredible Hulk" }).first();
+
+  await page.getByRole("img", { name: "The Incredible Hulk" }).first().click();
+
+  await expect(page.getByRole("heading", { name: "Rated: PG-" })).toBeVisible();
 });
